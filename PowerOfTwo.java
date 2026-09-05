ea66ebdb7a0fdb4d70f2bbd2ae114ae6f2cf41e1
Magam Venkata Srinuvasulu <magamsrinuvasulu@gmail.com>
2026-09-05 21:12:42 +0530

    Add files via upload

---
+++ PowerOfTwo.java
@@ -0,0 +1,14 @@
+class PowerOfTwo {
+    public static boolean isPowerOfTwo(int n) {
+        if(n<1){
+            return false;
+        }
+        if(n==1){
+            return true;
+        }
+        return n%2==0&&isPowerOfTwo(n/2);
+    }
+    public static void main(String[] args){
+        System.out.println(isPowerOfTwo(2));
+    }
+}
